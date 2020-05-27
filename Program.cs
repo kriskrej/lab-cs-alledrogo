d7cc990ff5bb7de267eddf2284b5d8be4c2f155a
Kris Krej <kris@krej.net>
2020-05-27 07:39:13 +0200

    Empty repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace lab_cs_alledrogo {
+    class Program {
+        static void Main(string[] args) {
+            var allegroKey = File.ReadAllText("allegro-klucz-WebAPI.txt");
+            var allegroSecret = File.ReadAllText("allegro-client-secret.txt");
+            Console.WriteLine(allegroKey);
+        }
+    }
+}
