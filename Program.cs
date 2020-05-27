e589e1a27f2da1f67fb3e8fde551c3731b39623b
Kris Krej <kris@krej.net>
2020-05-27 08:08:13 +0200

    clean up

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@ using System.IO;
 namespace lab_cs_alledrogo {
     class Program {
         static void Main(string[] args) {
-            var allegroKey = File.ReadAllText("allegro-klucz-WebAPI.txt");
-            var allegroSecret = File.ReadAllText("allegro-client-secret.txt");
-            Console.WriteLine(allegroKey);
+
         }
     }
 }
